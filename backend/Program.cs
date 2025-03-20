4fbe0223fbf7101108c048a0b495e9732b68a11a
root <root@Alan-laptop.>
2025-03-20 12:49:27 +0200

    Added backend workfolder

---
+++ backend/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace DockerHw4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Net.HttpListener listener = new System.Net.HttpListener();
+           listener.Prefixes.Add("http://localhost:8080/");
+           listener.Start();
+           Console.WriteLine("Starting host on http://localhost:8080/");
+           while (true)
+           {
+               HttpListenerContext context = listener.GetContext();
+               Console.WriteLine("Got request");
+               context.Response.StatusCode = 200;
+               context.Response.ContentType = "text/html";
+               context.Response.OutputStream.Write(System.Text.Encoding.UTF8.GetBytes("<h1>Hello World</h1>"));
+               context.Response.OutputStream.Close();
+               Console.WriteLine("Sent response");  
+           }
+        }
+    }
+}
